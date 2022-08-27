12d9c9de43948014c4610281c0b52216ccc21dae
Hongling Zhang <jolinhlzhang@gmail.com>
2022-08-26 23:10:56 -0600

    Staying logged In with Session

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const User = require('./models/user');
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const User = require("./models/user");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const session = require("express-session");
 
 mongoose
   .connect("mongodb://localhost:27017/authDemo", {
@@ -17,49 +18,55 @@ mongoose
     console.log(err);
   });
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(session({ secret: "notagoodsecret" }));
 
-app.get('/', (req, res) => {
-  res.send('THIS IS THE HOME PAGE')
-})
+app.get("/", (req, res) => {
+  res.send("THIS IS THE HOME PAGE");
+});
 
-app.get('/register', (req, res) => {
-  res.render('register')
-})
+app.get("/register", (req, res) => {
+  res.render("register");
+});
 
-app.post('/register', async (req, res) => {
+app.post("/register", async (req, res) => {
   const { password, username } = req.body;
   const hash = await bcrypt.hash(password, 12);
   const user = new User({
     username,
-    password: hash
-  })
+    password: hash,
+  });
   await user.save();
-  res.redirect('/')
-})
+  req.session.user_id = user._id;
+  res.redirect("/");
+});
 
-app.get('/login', (req, res) => {
-  res.render('login')
-})
+app.get("/login", (req, res) => {
+  res.render("login");
+});
 
-app.post('/login', async (req, res) => {
+app.post("/login", async (req, res) => {
   const { username, password } = req.body;
   const user = await User.findOne({ username });
   const validPassword = await bcrypt.compare(password, user.password);
-  if(validPassword) {
-    res.send('YAY WELCOME!')
+  if (validPassword) {
+    req.session.user_id = user._id;
+    res.redirect('/secret')
   } else {
-    res.send("TRY AGAIN!")
+    res.redirect('/login');
   }
-})
+});
 
-app.get('/secret', (req, res) => {
-  res.send('THIS IS SECRET! YOU CANNOT SEE ME UNLESS YOU ARE LOGGED IN!')
-})
+app.get("/secret", (req, res) => {
+  if (!req.session.user_id) {
+    res.redirect('/login')
+  }
+  res.send("THIS IS SECRET! YOU CANNOT SEE ME UNLESS YOU ARE LOGGED IN!");
+});
 
 app.listen(3000, () => {
-  console.log('SERVING YOUR APP!');
+  console.log("SERVING YOUR APP!");
 });
